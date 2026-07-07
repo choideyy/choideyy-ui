125915143ae867eb23817d32a52a34706c81bfb0
nixietran <ttngoc.nixe@gmail.com>
2026-07-07 15:42:28 +0200

    fix button recap

--- src/pages/Recap/Recap.tsx
+++ src/pages/Recap/Recap.tsx
@@ -20,13 +20,15 @@ export const Recap = () => {
 
       <main className="recap__hero">
         <h1>CHỌI DEYYY DANCE COMPETITION</h1>
-        <div className="recap__year-links">
-          <a className="recap__year-card" href="#year-2022">
-            <span>2022</span>
-          </a>
-          <a className="recap__year-card recap__year-card--accent" href="#year-2025">
-            <span>2025</span>
-          </a>
+        <div className="recap__year-wrapper">
+          <div className="recap__year-links">
+            <a className="recap__year-card" href="#year-2022">
+              <span>2022</span>
+            </a>
+            <a className="recap__year-card recap__year-card--accent" href="#year-2025">
+              <span>2025</span>
+            </a>
+          </div>
         </div>
       </main>
 
