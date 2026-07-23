917a013d7304015b04b21fc312b56283742ecd9d
hmdang <hieu-minh.dang@insa-lyon.fr>
2026-07-23 22:16:13 +0200

    add image mc-dj - iphone/tablet event layout adapt

--- src/components/McDjSection/defaultMembers.ts
+++ src/components/McDjSection/defaultMembers.ts
@@ -2,6 +2,7 @@ import mc1 from '../../assets/figma/recap/2025/mc-1.png';
 import mc2 from '../../assets/figma/recap/2025/mc-2.png';
 import type { McDjMember } from './types';
 
+
 export const DEFAULT_MC_DJ_MEMBERS: McDjMember[] = [
   {
     id: 'mc-1',
---
+++ src/components/McDjSection/evenMcDjMembers.ts
@@ -0,0 +1,15 @@
+import eventMC1 from '../../assets/figma/event/MCTed.png'
+import eventDJ from '../../assets/figma/event/DJHostic.png'
+import type { McDjMember } from './types';
+export const EVENT_MC_DJ_MEMBERS: McDjMember[] = [
+  {
+    id: 'mc-1',
+    image: eventMC1,
+    alt: 'EVENT MC',
+  },
+  {
+    id: 'dj-1',
+    image: eventDJ,
+    alt: 'EVENT DJ',
+  },
+];
