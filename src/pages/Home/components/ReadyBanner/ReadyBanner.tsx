859097f24a52a915221dabf4d32a5947eaa4d5e0
hmdang <hieu-minh.dang@insa-lyon.fr>
2026-07-27 22:46:47 +0200

    fix banner home

--- src/pages/Home/components/ReadyBanner/ReadyBanner.tsx
+++ src/pages/Home/components/ReadyBanner/ReadyBanner.tsx
@@ -1,10 +1,15 @@
 import './ReadyBanner.css';
+import readyBannerImg from '../../../../assets/banner_ready.png';
 
 export const ReadyBanner = () => {
   return (
     <div className="ready-banner">
-      <div className="ready-banner__shadow" aria-hidden="true" />
-      <div className="ready-banner__bar" aria-hidden="true" />
+      <img
+        className="ready-banner__image"
+        src={readyBannerImg}
+        alt=""
+        aria-hidden="true"
+      />
       <p className="ready-banner__text">ARE YOU READY ?</p>
     </div>
   );
